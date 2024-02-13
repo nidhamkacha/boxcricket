2a30a451b5bdbec7beb46e95b30c2a8f55997db0
vijay vekariya <vijayvekariya007@gmail.com>
2024-02-13 15:45:12 +0530

    13-2-24

---
+++ lib/onboding_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class onbodingScreen extends StatefulWidget {
+  const onbodingScreen({super.key});
+
+  @override
+  State<onbodingScreen> createState() => _onbodingScreenState();
+}
+
+class _onbodingScreenState extends State<onbodingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
