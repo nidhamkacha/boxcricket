e37b03e192a21ba7ec1dc27158a77634d252786b
vijay vekariya <vijayvekariya007@gmail.com>
2024-02-13 16:04:25 +0530

    12-3-24

--- lib/onboding_screen.dart
+++ lib/onboding_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class onbodingScreen extends StatefulWidget {
   const onbodingScreen({super.key});
@@ -11,7 +12,23 @@ class _onbodingScreenState extends State<onbodingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  "SKIP",
+                  style: GoogleFonts.poppins(
+                      color: Color(0XFF78A408), fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
