78f6492f3d6a39e03e85a2cc59fb08e93f39b6ed
vijay vekariya <vijayvekariya007@gmail.com>
2024-02-14 15:53:37 +0530

    14-2-24

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -11,7 +12,39 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 75,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.arrow_back,
+                size: 24,
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              Text(
+                "LOGIN TO ACCOUNT",
+                style: GoogleFonts.poppins(
+                    fontSize: 30, fontWeight: FontWeight.w700),
+              )
+            ],
+          ),
+          Column(
+            children: [
+              Text(
+                "Your Full Name",
+                style: GoogleFonts.poppins(
+                    fontSize: 30, fontWeight: FontWeight.w500),
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
