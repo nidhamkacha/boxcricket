9a4271225506283094090a96553b3ba275599ee4
vijay vekariya <vijayvekariya007@gmail.com>
2024-02-17 14:56:02 +0530

    17-2-23

--- lib/HomeScreens/details_screen_package.dart
+++ lib/HomeScreens/details_screen_package.dart
@@ -21,6 +21,7 @@ class _DetailScreenState extends State<DetailScreen> {
         ),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Padding(
             padding: const EdgeInsets.all(8.0),
@@ -35,7 +36,177 @@ class _DetailScreenState extends State<DetailScreen> {
                 border: Border.all(),
               ),
             ),
-          )
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(2.0),
+                child: Container(
+                  height: 5,
+                  width: 5,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle, color: Color(0XFF20AC2A)),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(2.0),
+                child: Container(
+                  height: 5,
+                  width: 5,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle, color: Color(0XFF707070)),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(2.0),
+                child: Container(
+                  height: 5,
+                  width: 5,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle, color: Color(0XFF707070)),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(2.0),
+                child: Container(
+                  height: 5,
+                  width: 5,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle, color: Color(0XFF707070)),
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  height: 62,
+                  width: 75,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage("assets/images/Vector (1).png"))),
+                ),
+                Container(
+                  height: 62,
+                  width: 75,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage("assets/images/Vector (2).png"))),
+                ),
+                Container(
+                  height: 62,
+                  width: 75,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage("assets/images/Vector (3).png"))),
+                ),
+                Container(
+                  height: 62,
+                  width: 75,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage("assets/images/Vector (4).png"))),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "Capital Cricket Club, Mota Varachha",
+              style: GoogleFonts.poppins(
+                fontSize: 16,
+                fontWeight: FontWeight.w700,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(2.0),
+            child: Row(
+              children: [
+                Icon(
+                  Icons.location_on_outlined,
+                  color: Color(0XFF78A408),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Capital Box, Cricket Managed By Deep",
+                          style: GoogleFonts.poppins(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.black87)),
+                      Text("Academy, Near Maharaja Farm",
+                          style: GoogleFonts.poppins(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.black87)),
+                      Text("Mota Varachha, Surat, Gujarat",
+                          style: GoogleFonts.poppins(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.black87)),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Icon(
+                  Icons.star,
+                  color: Color(0XFF20AC2A),
+                ),
+                Icon(
+                  Icons.star,
+                  color: Color(0XFF20AC2A),
+                ),
+                Icon(
+                  Icons.star,
+                  color: Color(0XFF20AC2A),
+                ),
+                Icon(
+                  Icons.star,
+                  color: Color(0XFF20AC2A),
+                ),
+                Icon(
+                  Icons.star,
+                  color: Color(0XFFB5CFE1),
+                ),
+                SizedBox(
+                  width: 9,
+                ),
+                Text(
+                  "Good",
+                  style: GoogleFonts.poppins(
+                      color: Color(0XFF20AC2A),
+                      fontSize: 10,
+                      fontWeight: FontWeight.w700),
+                ),
+                SizedBox(
+                  width: 9,
+                ),
+                Text(
+                  "62,778 Ratings",
+                  style: GoogleFonts.poppins(
+                      color: Color(0XFF000000),
+                      fontSize: 10,
+                      fontWeight: FontWeight.w400),
+                ),
+              ],
+            ),
+          ),
+          
         ],
       ),
     );
