3e9fe730f33984adf13ae7e61d9588694f3b2c5a
Nidham Kacha <nidham78@gmail.com>
2024-02-20 19:21:23 +0530

    22-2-24
    Signed-off-by: Nidham Kacha <nidham78@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,9 @@ import 'package:boxcricket/onbodingScreens/onboding_screen.dart';
 import 'package:boxcricket/onbodingScreens/onboding_screen_second.dart';
 import 'package:flutter/material.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 void main() {
   runApp(const MyApp());
 }
