0b2b351f7af5f2ada34ff427e5176ad8cf6f46cf
vijay vekariya <vijayvekariya007@gmail.com>
2024-02-20 15:44:15 +0530

    20-2-24

--- lib/EventsScreen/Filter_Screen.dart
+++ lib/EventsScreen/Filter_Screen.dart
@@ -9,6 +9,7 @@ class FilterScreen extends StatefulWidget {
 }
 
 class _FilterScreenState extends State<FilterScreen> {
+  double _currentSliderValue = 10;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -140,11 +141,73 @@ class _FilterScreenState extends State<FilterScreen> {
                   fontWeight: FontWeight.w700),
             ),
           ),
+          Slider(
+            value: _currentSliderValue,
+            max: 100,
+            thumbColor: Colors.white,
+            activeColor: Color(0xff78A408),
+            inactiveColor: Color(0xff787880),
+            divisions: 100,
+            label: _currentSliderValue.round().toString(),
+            onChanged: (double value) {
+              setState(() {
+                _currentSliderValue = value;
+              });
+            },
+          ),
+          Divider(
+            indent: 10,
+            endIndent: 10,
+            color: Colors.black,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "DATE",
+              style: GoogleFonts.poppins(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w700),
+            ),
+          ),
           Container(
-            height: 4,
-            width: 311,
-            color: Color(0XFF78A408),
-          )
+            margin: EdgeInsets.all(4),
+            height: 52,
+            width: double.infinity,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Select Date",
+                    style: GoogleFonts.poppins(
+                        color: Colors.black45,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  Icon(Icons.calendar_month)
+                ],
+              ),
+            ),
+            decoration: BoxDecoration(
+                border: Border.all(), borderRadius: BorderRadius.circular(10)),
+          ),
+          Divider(
+            indent: 10,
+            endIndent: 10,
+            color: Colors.black,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "AMENITIES",
+              style: GoogleFonts.poppins(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w700),
+            ),
+          ),
         ],
       ),
     );
